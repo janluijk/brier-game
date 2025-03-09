4dd522b878004e6cddf195fb04242ec8f12c3c71
Jan Luijk <luijk.jan@gmail.com>
2025-03-09 11:32:22 +0100

    Add types to predictions.py for code readability

--- predictions.py
+++ predictions.py
@@ -1,24 +1,30 @@
-from expert_performance import ExpertPerformanceTracker
+from expert_performance import ExpertPerformanceTracker, ProbabilityDistribution
 import numpy as np 
 from scipy.optimize import fsolve
+from typing import Dict, List
+
+ExpertName = str
+ProbabilityDistribution = List[float]
+Predictions = Dict[ExpertName, ProbabilityDistribution]
 
 class PredictionOptimizer:
     def __init__(self, expert_manager: ExpertPerformanceTracker):
         self.expert_manager = expert_manager 
         self.outcome_space = self.expert_manager.outcome_space
 
-    def log_weighted_loss(self, outcome, expert_predictions):
+    def log_weighted_loss(self, outcome: int, expert_predictions: Predictions) -> float:
         weights = self.expert_manager.weights
         brier_score = self.expert_manager.brier_score
 
         total_weighted_loss = sum(
-            weights[expert] * np.exp(-brier_score(outcome, predictions))
+            weights[expert] * np.exp(- brier_score(outcome, predictions))
             for expert, predictions in expert_predictions.items()
         )
         return -np.log(total_weighted_loss)
 
-    def get_optimal_predictions(self, expert_predictions):
-        def loss_equation(x):
+    def get_optimal_predictions(self, expert_predictions: Predictions) -> ProbabilityDistribution:
+
+        def loss_equation(x: float) -> float:
             return sum(
                 max(x - self.log_weighted_loss(outcome, expert_predictions), 0.0) 
                 for outcome in self.outcome_space
